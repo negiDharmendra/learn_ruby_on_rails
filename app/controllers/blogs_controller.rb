3a59a3d212ca36a1367b37e45fb19bf95194d0a3
Dharmendra Singh <dharmenn@thoughtworks.com>
2016-04-14 18:42:08 +0530

    hello

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,23 @@
 class BlogsController < ApplicationController
+    def create
+        @user = User.find_by_id(params[:user_id])
+        @blog = @user.blogs.create(params.require(:blog).permit(:title, :content))
+        redirect_to user_path(@user)
+    end
+
+    def edit
+    end
+
+    def new
+      # require 'pry'; binding.pry
+      @user = User.find_by_id(params[:user_id])
+      @blog = @user.blogs.new
+    end
+
+    def destroy
+        @user = User.find_by_id(params[:user_id])
+        @blog = @user.blogs.find_by(id: params[:id])
+        @blog.destroy
+        redirect_to user_path(@user)
+    end
 end
--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,27 +1,23 @@
 require 'rails_helper'
 
 describe 'Blog' do
-    # before(:each) do
-    #     @user = User.new(:michael)
-    #     @user.blog.build(title: 'ruby on rails', content: 'Ruby on Rails, or simply Rails, is a web application framework written in Ruby under the MIT License.')
-    # end
-    #
-    # test 'should be valid' do
-    #     expect(@blog.valid?).to eq(true)
-    # end
-    # # #
-    # test "user id should be present" do
-    #   @micropost.user_id = nil
-    #   assert_not @micropost.valid?
-    # end
-    #
-    # test "content should be present" do
-    #   @micropost.content = "   "
-    #   assert_not @micropost.valid?
-    # end
-    #
-    # test "content should be at most 140 characters" do
-    #   @micropost.content = "a" * 141
-    #   assert_not @micropost.valid?
-    # end
+
+    test 'should be valid' do
+        expect(@blog.valid?).to eq(true)
+    end
+    # #
+    test "user id should be present" do
+      @micropost.user_id = nil
+      assert_not @micropost.valid?
+    end
+
+    test "content should be present" do
+      @micropost.content = "   "
+      assert_not @micropost.valid?
+    end
+
+    test "content should be at most 140 characters" do
+      @micropost.content = "a" * 141
+      assert_not @micropost.valid?
+    end
 end
