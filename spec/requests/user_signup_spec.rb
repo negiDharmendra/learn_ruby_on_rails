8490127f758969f3407fe9401f7f5d01f4e178ec
Dharmendra Singh <dharmenn@thoughtworks.com>
2016-04-14 08:42:17 +0530

    I'v wrote two test for user signup

---
+++ spec/requests/user_signup_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'User Signup', type: :request do
+
+    it 'error message should be there for invalid signup' do
+        user  =  FactoryGirl.create(:user)
+        get '/signup'
+        expect(response).to render_template('users/new')
+        post '/users', user: {name: 'xyz',email: 'john_1@gmail.com',password: 'password',password_confirmation: 'password'}
+        expect(response.body.include?('error_explanation')).to eq(true)
+        expect(response).to render_template('users/new')
+        expect(response).to have_http_status(200)
+        user.destroy
+    end
+    it 'error message should be there for invalid signup' do
+        get '/signup'
+        expect(response).to render_template('users/new')
+        post '/users', user: {name: 'xyz',email: 'new_john_1@gmail.com',password: 'password',password_confirmation: 'password'}
+        expect(response.body.include?('error_explanation')).to eq(false)
+        expect(response).to have_http_status(302)
+        p response.body
+    end
+end
