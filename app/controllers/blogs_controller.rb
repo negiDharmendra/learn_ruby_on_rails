8ed6bc5da4130ecb8c8ef59c462fe40dc73ce08a
Dharmendra Singh <dharmenn@thoughtworks.com>
2016-04-15 16:04:53 +0530

    [dharam] 	implemented show more option for all user for a blog 	and edit and remove a blog feature just for a user who has owned 	that blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,23 +1,59 @@
 class BlogsController < ApplicationController
+  attr_accessor :id
+
+  def new
+    get_blog
+  end
+
   def create
     @user = User.find_by_id(params[:user_id])
-    @blog = @user.blogs.create(params.require(:blog).permit(:title, :content))
+    blog_params
     redirect_to user_path(@user)
   end
 
   def show
+    # require 'pry'; binding.pry
+    get_blog
+    render 'show'
   end
 
-  def new
-    @user = User.find_by_id(params[:user_id])
-    @blog = @user.blogs.new
+  def show_for_all
+    require 'pry'; binding.pry
+    @blog = Blog.find_by_id(params[:id])
+    render 'show'
   end
+
+  def edit
+    get_blog
+    render 'blogs/edit'
+  end
+
+  def update
+    get_blog
+    @blog.update blog_params
+    redirect_to user_path (@user)
+  end
+
 
   def destroy
     # require 'pry'; binding.pry
-    @user = User.find_by_id(params[:user_id])
-    @blog = @user.blogs.find_by(id: params[:id])
+    get_blog
     @blog.destroy
     redirect_to user_path(@user)
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+
+  private
+  def get_blog
+    if logged_in?
+      @user = User.find_by_id(params[:user_id])
+      @blog = @user.blogs.find_by_id(params[:id])
+    else
+      @blog = Blog.find_by_id(params[:id])
+    end
   end
 end
