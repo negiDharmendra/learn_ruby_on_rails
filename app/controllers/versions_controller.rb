77b30d5cde1355aa7eda7eff1579576e611e52cd
ptyagi <ptyagi@INptyagi.local>
2016-04-18 11:57:26 +0530

    #Priyanka - Adding view for displaying history of a blog

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -1,2 +1,14 @@
 class VersionsController < ApplicationController
+
+  def show
+    get_version
+       render 'show'
+
+  end
+
+  private
+  def get_version
+    @blog = Blog.find_by_id(params[:blog_id])
+    @version = @blog.versions.find_by_id(params[:id])
+  end
 end
