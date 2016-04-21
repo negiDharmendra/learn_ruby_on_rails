ab100670383c5c54f02962976af93df9684e7406
ptyagi <ptyagi@INptyagi.local>
2016-04-21 15:07:17 +0530

    testing functionality of edit action of account activation controller

--- spec/controllers/account_activations_controller_spec.rb
+++ spec/controllers/account_activations_controller_spec.rb
@@ -3,11 +3,21 @@ require 'rails_helper'
 RSpec.describe AccountActivationsController, type: :controller do
   let(:user) { create(:user) }
   describe 'GET#edit' do
-    it 'should activate the account' do
-      expect(user.activated).to eq(false)
-      get :edit, id: user.activation_token, email: user.email
-      expect(flash[:success_message].nil?).to eq(false)
-      expect(response).to redirect_to '/login'
+    context "when activation link is valid" do
+      it 'should activate the account' do
+        expect(user.activated).to eq(false)
+        get :edit, id: user.activation_token, email: user.email
+        expect(flash[:success_message].nil?).to eq(false)
+        expect(response).to redirect_to '/login'
+      end
+    end
+    context "when activation link is invalid" do
+      it 'should throw an error and redirect to root page' do
+        get :edit, id: 'invalid_id', email: user.email
+        expect(flash[:danger].nil?).to eq(false)
+        expect(response).to redirect_to root_url
+
+      end
 
     end
   end
---
+++ spec/controllers/blog_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Blog Controller, type: :controller do
+
+  it 'should do something' do
+
+    true.should == false
+  end
+end
